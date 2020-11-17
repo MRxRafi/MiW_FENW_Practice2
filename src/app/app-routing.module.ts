4e8bf799cc8f3336179c3af2d15e58ea36501052
MRxRafi <rafadriveclase@gmail.com>
2020-11-17 11:21:54 +0100

    Initial with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {PreferencesComponent} from './preferences/preferences.component';
+import {PlayComponent} from './play/play.component';
+import {ScoresComponent} from './scores/scores.component';
+import {SignupComponent} from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'preferencias', component: PreferencesComponent },
+  { path: 'jugar', component: PlayComponent },
+  { path: 'puntuaciones', component: ScoresComponent },
+  { path: 'registro', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { PlayComponent } from './play/play.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { ScoresComponent } from './scores/scores.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    PlayComponent,
+    PreferencesComponent,
+    ScoresComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
